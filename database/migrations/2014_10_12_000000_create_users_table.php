8365dffe90f42c5273e693e764898538548899cb
lnlockly <lnlockly@gmail.com>
2021-07-12 15:52:06 +0000

    migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,33 +4,37 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+class CreateUsersTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('users', function (Blueprint $table) {
+			$table->id();
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->integer('nof_stamps');
+			$table->integer('nof_gifts');
+			$table->boolean('webpush');
+			$table->string('email')->unique();
+			$table->timestamp('email_verified_at')->nullable();
+			$table->string('password');
+			$table->rememberToken();
+			$table->integer('card_id')->unsigned();
+			$table->foreign('card_id')->references('id')->on('cards')->onDelete('cascade');
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('users');
+	}
 }
--- database/migrations/2021_07_12_010801_create_cards_table.php
+++ database/migrations/2021_07_12_010801_create_cards_table.php
@@ -4,30 +4,38 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCardsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('cards', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->integer('client_id')->unsigned();
-            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade')
-        });
-    }
+class CreateCardsTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('cards', function (Blueprint $table) {
+			$table->id();
+			$table->string('name');
+			$table->string('logo');
+			$table->string('color_header');
+			$table->string('body_header');
+			$table->integer('stamps');
+			$table->string('stamp_icon');
+			$table->string('condition');
+			$table->string('description');
+			$table->string('card_use');
+			$table->string('contact');
+			$table->integer('client_id')->unsigned();
+			$table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+			$table->timestamps();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('cards');
-    }
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('cards');
+	}
 }
