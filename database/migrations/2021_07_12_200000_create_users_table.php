ee087fb0eb47a2a83226487d13b2ee16868d1d31
lnlockly <lnlockly@gmail.com>
2021-08-27 20:50:59 +0300

    a

--- database/migrations/2021_07_12_200000_create_users_table.php
+++ database/migrations/2021_07_12_200000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration {
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->rememberToken();
-			$table->foreignId('card_id')->constrained();
+			$table->foreignId('card_id')->constrained()->onDelete('cascade');
 
 			$table->timestamps();
 		});
--- database/migrations/2021_08_27_162425_create_questions_table.php
+++ database/migrations/2021_08_27_162425_create_questions_table.php
@@ -4,28 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuestionsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('questions', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+class CreateQuestionsTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('questions', function (Blueprint $table) {
+			$table->id();
+			$table->string('name');
+			$table->mediumText('text');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('questions');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('questions');
+	}
 }
